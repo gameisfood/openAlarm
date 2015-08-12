535893c6e0776bd95bb6eb7d6c9970eaa00f0e47
James Adams <jamesroyadams@hotmail.co.uk>
2015-08-12 20:33:41 +0000

    hello world server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-console.log("Not ready yet bruv");
+console.log("Starting api server");
+
+var express = require('express');
+var app = express();
+var port = 9090;
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+console.log("Server Running on Port " + port);
+app.listen(port);
