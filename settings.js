a6d1706421c3b0b6b9fdaee59c98ee3976a0b43d
James Adams <jamesroyadams@hotmail.co.uk>
2015-08-13 08:39:35 +0000

    Adding global settings file

---
+++ settings.js
@@ -0,0 +1,9 @@
+// Exported variables
+
+exports.REDIS_PORT = 6379;
+exports.REDIS_URI = 'bagmilk.mooo.com';
+exports.REDIS_PASSWORD = "none at the moment";
+
+exports.API_PORT = 9090;
+exports.API_IP = "bagmilk.mooo.com"; //not yet forwarded
+exports.API_KEY = "none at the moment";
