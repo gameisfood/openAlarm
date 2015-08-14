f022d170ccb7b7286e686c46ad468c3825e86791
root <root@raspberrypi.(none)>
2015-08-14 16:07:34 +0000

    added radis in alarm.js

--- alarm/alarm.js
+++ alarm/alarm.js
@@ -1,2 +1,15 @@
+var redis = require('redis');
+var settings = require('../settings.js');
+var client = redis.createClient(settings.REDIS_URI , settings.REDIS_PORT);
+
 console.log("Wake up please?");
 console.log("No snooge.!");
+
+client.on('connect', function() {
+    console.log('connected');
+});
+client.on('error',function() {
+    console.log('Error connecting radis');
+});
+
+//add an alarm ex(without json)
