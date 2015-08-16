ffbb9bb8876bea8100335d96ff1bef5b88fcbda6
sidharth-agarwal <root@raspberrypi.(none)>
2015-08-16 17:25:18 +0530

    Added cron to play woodkid every minute

--- alarm/alarm.js
+++ alarm/alarm.js
@@ -1,8 +1,8 @@
 var redis = require('redis');
-var settings = require('../settings.js');
+var settings = require('../shared_resources/settings.js');
 var client = redis.createClient();//settings.REDIS_URI , settings.REDIS_PORT);
 var trigger = require('./alarmTrigger.js');
-    //trigger = new alarmTrigger();
+var CronJob = require('cron').CronJob;
 
 console.log("Wake up please?");
 console.log("No snooge.!");
@@ -17,9 +17,15 @@ client.on('error',function(err) {
 //add an alarm ex(without json)
 client.set("01","09:00");
 console.log("Set example");
+/*
 console.log("Getting alarm at 01: ");
 client.get("01", function (err, reply) {
         console.log(reply.toString());
     });
-var ringer = trigger.ring;
-ringer("01");
+    */
+
+//add a cronjob for the above time
+new CronJob('0 * * * * *', function() {
+    trigger.ring("01");
+      //console.log('You will see this message every second');
+    }, null, true, null);
--- alarm/alarmTrigger.js
+++ alarm/alarmTrigger.js
@@ -1,9 +1,10 @@
 var redis = require('redis') ,
-    settings = require('../settings.js');
+    settings = require('../shared_resources/settings.js');
 var Player = require('node-mpg123'),
-    player = new Player('../woodKid1.mp3');
+    player = new Player('../woodKid.mp3');
+var delayed = require('delayed');
 
-
+var finishedPlaying;
 var ring = function(id) {
     var client = redis.createClient();//settings.REDIS_URI , settings.REDIS_PORT);
     //json id holder should go here.. playing from the relevent src and stuff..
@@ -11,17 +12,18 @@ var ring = function(id) {
             var jid = reply.toString();
             console.log("Ring Ring @jid:", jid);
             player.play();
+            finishedPlaying = false;
         });
 
-        player.on('playing',function(item){
-          console.log('im playing... src:' + item);
-        });
-        player.on('error', function(err){
-          // when error occurs
-          console.log(err);
-        });
+        //check if the player has finished playing too soon.. if so there was an error with the media file.
+        delayed.delay(function () {
+          if(finishedPlaying === true)
+            console.log("Error playing media file.");
+        }, 1000)
+
         player.on('complete', function () {
             console.log('Done with alarm playback!');
+            finishedPlaying = true;
         });
 }
 exports.ring = ring;
