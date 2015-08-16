7b4fefc5edc26adf51af55c40ff99ca760901dbe
James Adams <jamesroyadams@hotmail.co.uk>
2015-08-16 19:44:37 +0000

    abstracts redis from api

---
+++ apiAlarm/redis-messenger.js
@@ -0,0 +1,34 @@
+var redis = require('redis');
+var settings = require('../shared_resources/settings.js')
+
+var client = redis.createClient(settings.REDIS_PORT,settings.REDIS_URI, {});
+
+client.on("connect", function () {
+       console.log("redis-messenger.js : API Server connected to REDIS");
+       console.log("redis-messenger.js : Port : " + settings.REDIS_PORT);
+       console.log("redis-messenger.js : URI : " + settings.REDIS_URI);
+   });
+
+client.on("error", function (err) {
+    console.log("redis-messenger.js : " + err);
+});
+
+exports.addNewAlarm = function(alarmProperties) {
+  client.INCR("uniqueKey");
+  client.get("uniqueKey", function (err, reply) {
+    console.log("Adding new alarm with ID : " + reply);
+    client.set(reply, alarmProperties);
+  });
+};
+
+exports.getAlarmByID = function(alarmID) {
+  client.get("alarmID", function (err, reply) {
+    console.log(reply);
+  });
+};
+
+exports.deleteAlarmByID = function(alarmID) {
+  client.del("alarmID", function (err, reply) {
+    console.log(reply);
+  });
+};
