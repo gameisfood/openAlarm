2cce2a778d5b281c887f08a945e2dce083198ab7
James Adams <jamesroyadams@hotmail.co.uk>
2015-08-18 14:25:35 +0000

    Server will now serve an alarm template as json

--- apiAlarm/server.js
+++ apiAlarm/server.js
@@ -2,10 +2,25 @@ console.log("Starting api server");
 
 var express = require('express');
 var app = express();
+var router = express.Router();
 var port = 9090;
-app.get('/', function(req, res){
-  res.send('hello world');
+var alarm_util = require("./redis-messenger.js");
+var alarmTemplateJSON = require("../shared_resources/alarmTemplate.json");
+app.get('/api', function(req, res){
+  res.send('This is the openAlarm API, for docs go to http://loltheresnodocswhodoyouthinkiam.com');
 });
 
+var serveTemplate = router.route('/alarm/template');
+serveTemplate.get(function(req, res) {
+  res.json(alarmTemplateJSON);
+})
+
+var addAlarm = router.route('/alarm/add');
+addAlarm.post(function(req, res) {
+  var alarm = req.body.alarm;
+
+});
+
+app.use('/api', router)
 console.log("Server Running on Port " + port);
 app.listen(port);
